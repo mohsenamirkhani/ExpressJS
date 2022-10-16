3f6c52647d5224daed074962b40cd7fbe7d096e8
mohsen <mohsen.amirkhani@live.com>
2022-10-16 11:16:07 +0330

    05 Working with Express.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -7,12 +9,13 @@ const shopRoute = require("./routes/shop");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(adminRoute);
+app.use('/admin', adminRoute);
 app.use(shopRoute);
 
 app.use((req, res) => {
-    res.status(404).send("<h1>Page Not Found</h1>")
+    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 })
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,24 @@
+const path = require("path");
+
 const express = require("express");
 
-const router = express.Router();
-
-router.get("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='/product' method='POST'><input type='text' name='title'><button>Add Product</button></form>"
-  ); // send the response
-});
+const rootDir = require("../helpers/path");
 
 //app.get , app.post , app.put and app.delete are the other forms of app.use but only triggers for that specific method
-router.post("/product", (req, res) => {
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get("/add-product", (req, res, next) => {
+  const absolutPath = path.join(rootDir, 'views', 'add-product.html');
+  res.sendFile(absolutPath);
+});
+
+
+// /admin/add-product => POST
+router.post("/add-product", (req, res) => {
   const body = req.body;
   console.log(body);
-  res.redirect("/add-product");
+  res.redirect("/admin/add-product");
 });
 
 module.exports = router;
